6e43952934d87999caf6311d810920209df6278c
mposso7 <micposso@gmail.com>
2017-01-31 01:48:43 -0500

    added links to labs page

--- labs.php
+++ labs.php
@@ -9,7 +9,7 @@
         <div class="row">
           <div class="col-md-6 project-container">
             <h1 class="projects-lab"><span class="highlight">~</span>HTML module exporter</h1>
-            <p class="projects-text-labs">I built this tool to help email developers export their moduels as individual HTML files. It uses JSzip a library that saves and exports files as zip packages. <span class="github-link"><a href="">See it on GitHub</a></span></p>
+            <p class="projects-text-labs">I built this tool to help email developers export their moduels as individual HTML files. It uses JSzip a library that saves and exports files as zip packages. <span class="github-link"><a href="https://github.com/micposso/javascript-HTML-module-export">See it on GitHub</a></span></p>
             <div class="container">
               <ul class="nav navbar-nav navbar-left tool-bar">
                 <li><img src="img/HTML5-logo.svg" width="39" class="icon-tool"></li>
@@ -20,7 +20,7 @@
           </div>
           <div class="col-md-6 project-container">
             <h1 class="projects-lab"><span class="highlight">~</span>GitHub Profile Match</h1>
-            <p class="projects-text-labs">This tool will find other developers with the same interest. Thsi project is currently inder development and hosted on GitHub. It is using Angular2 and the GitHub API. <span class="github-link"><a href="">See it on GitHub</a></span></p>
+            <p class="projects-text-labs">This tool will find other developers with the same interest. Thsi project is currently inder development and hosted on GitHub. It is using Angular2 and the GitHub API. <span class="github-link"><a href="https://github.com/micposso/GitHug">See it on GitHub</a></span></p>
             <div class="container">
               <ul class="nav navbar-nav navbar-left tool-bar">
                 <li><img src="img/github.svg" width="55" class="icon-tool"></li>
@@ -34,7 +34,7 @@
         <div class="row">
           <div class="col-md-6 project-container">
             <h1 class="projects-lab"><span class="highlight">~</span>Spotify Player</h1>
-            <p class="projects-text-labs">This is an audio player that uses the Spotify API to pull a live search feed of songs. It is using the native HTML5 video player and angular2. <span class="github-link"><a href="">See it on GitHub</a></span></p>
+            <p class="projects-text-labs">This is an audio player that uses the Spotify API to pull a live search feed of songs. It is using the native HTML5 video player and angular2. <span class="github-link"><a href="https://github.com/micposso/Angular-Spotify-App">See it on GitHub</a></span></p>
             <div class="container">
               <ul class="nav navbar-nav navbar-left tool-bar">
                 <li><img src="img/github.svg" width="55" class="icon-tool"></li>
@@ -45,7 +45,7 @@
           </div>
           <div class="col-md-6 project-container">
             <h1 class="projects-lab"><span class="highlight">~</span>Materialize Jekyll Theme</h1>
-            <p class="projects-text-labs">This is a theme for the Jekyll static content management system using the Materialize framework. I used SASS for development and gulp for task running <span class="github-link"><a href="">See it on GitHub</a></span></p>
+            <p class="projects-text-labs">This is a theme for the Jekyll static content management system using the Materialize framework. I used SASS for development and gulp for task running <span class="github-link"><a href="https://github.com/micposso/materialize-jekyll-theme">See it on GitHub</a></span></p>
             <div class="container">
               <ul class="nav navbar-nav navbar-left tool-bar">
                 <li><img src="img/js.svg" width="55" class="icon-tool"></li>
