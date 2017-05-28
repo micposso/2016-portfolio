0f9f1961d1f60707f1f39bfb0a54cd737973bbb7
mposso7 <micposso@gmail.com>
2017-05-27 22:54:31 -0400

    added link to nav and fixed description in education section

--- education.php
+++ education.php
@@ -4,17 +4,17 @@
       <div class="col-md-12 col-centered">
         <div class="projects-text-container">
           <h1 class="projects-headline">Technology Education</h1>
-          <p class="projects-text">In an effort to stay up-to-date with the technologies available, I'm contantly building projects based on online tutorials or just based on ideas of my own. This is a collection of those projects. Feel free to collaborate and contribute on GitHub. Some of this projects are still in progress.</p>
+          <p class="projects-text">As a compulsive self-learner, I believe that with dedication and the correct help, anybody can learn the technology skills they need to advance their careers. For more than 7 years I have organized educational workshops and professional networking using the social platform MeetUp. Moreover, I have written and developed tutorials and professional level technology educational courses. If your company is interested in developing a technology program, please contact me at <span class="github-link">micposso@gmial.com</span></p>
         </div>
         <div class="row thumbs">
           <div class="col-md-6 project-container">
             <h1 class="projects-lab"><span class="highlight">~</span>Founder Latino WebDevs MeetUp</h1>
-            <p class="projects-text-labs">This tool will find other developers with the same interest. Thsi project is currently inder development and hosted on GitHub. It is using Angular2 and the GitHub API. <span class="github-link"><a href="https://www.meetup.com/Latino-Web-Developers-NYC/">MeetUp</a></span></p>
+            <p class="projects-text-labs">Organizer of a community of technology oriented individuals to foster collaboration and mutual learning. The workshops are conducted in live workshop format. <span class="github-link"><a href="https://www.meetup.com/Latino-Web-Developers-NYC/">MeetUp</a></span></p>
             <a href=""><img src="img/meetup.jpg" class="img-responsive"></a>
           </div>
           <div class="col-md-6 project-container">
             <h1 class="projects-lab"><span class="highlight">~</span>Online Instructor Platzi</h1>
-            <p class="projects-text-labs">I built this tool to help email developers export their moduels as individual HTML files. It uses JSzip a library that saves and exports files as zip packages. <span class="github-link"><a href="https://platzi.com/clases/diseno-email/">Platzi</a></span></p>
+            <p class="projects-text-labs">Created an online video course in modern HTML development for email marketing. Designed course materials and hands on exercises for the students. <span class="github-link"><a href="https://platzi.com/clases/diseno-email/">Platzi</a></span></p>
             <a href=""><img src="img/platzi.jpg" class="img-responsive"></a>
           </div>
         </div>
